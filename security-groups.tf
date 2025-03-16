60be82951b1f3922029d2c11b668883dca251ac2
Harish <sbvh1437@gmail.com>
2025-03-16 12:06:00 +0000

    alb, ec2, prov, sgs, vars, outs, vpc files have added

---
+++ security-groups.tf
@@ -0,0 +1,22 @@
+# Create Security Group
+resource "aws_security_group" "demo_sg" {
+  vpc_id = aws_vpc.demo_vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "demo-security-group"
+  }
+}
